6f8abdf2df0fba57a3e0222a35be566e77797e8b
REDDY PRASAD <dev.drprasad@aim.com>
2020-05-10 22:03:17 +0530

    :bug: Dont allow manually execution of plugin

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/dev-drprasad/porter-hashicorp-plugins/pkg"
 	"github.com/spf13/cobra"
 )
@@ -13,15 +16,15 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	// si, err := os.Stdin.Stat()
-	// if err != nil {
-	// 	fmt.Println("could not get stdin info")
-	// 	os.Exit(1)
-	// }
-	// if si.Mode()&os.ModeCharDevice == os.ModeCharDevice {
-	// 	fmt.Println("This binary is a Porter plugin. It is not meant to be executed directly.")
-	// 	os.Exit(126)
-	// }
+	si, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Println("could not get stdin info")
+		os.Exit(1)
+	}
+	if si.Mode()&os.ModeCharDevice == os.ModeCharDevice {
+		fmt.Println("This binary is a Porter plugin. It is not meant to be executed directly.")
+		os.Exit(126)
+	}
 	p = pkg.New()
 	rootCmd.SetErr(p.Err)
 	rootCmd.AddCommand(versionCmd, runCmd)
